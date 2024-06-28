5d16afc0c813d954dc3715266ff18972c6d2564d
swaggp <mutebinuhu1@gmail.com>
2024-06-28 06:27:36 +0400

    chore: few changes

--- app/components/OurTeam.jsx
+++ app/components/OurTeam.jsx
@@ -49,7 +49,7 @@ const OurTeam = () => {
       </div>
       <div className='text-center m-12 text-white'>
       <Link href="#booknow" className='bg-[#D5A354] p-4 font-bold rounded px-6'>
-      Book Mobile Saloon Now 
+      Book Mobile Saloon 
                 </Link>
     </div>
     </div>
